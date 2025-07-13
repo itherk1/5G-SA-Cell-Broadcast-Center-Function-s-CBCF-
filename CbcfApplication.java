d062b9210a636808f6abcbbf475e81576325b53c
Rishabh Kankane <36306962+itherk1@users.noreply.github.com>
2025-07-13 13:52:15 +0530

    Add files via upload

---
+++ CbcfApplication.java
@@ -0,0 +1,11 @@
+package com.example.cbcf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbcfApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CbcfApplication.class, args);
+    }
+}
